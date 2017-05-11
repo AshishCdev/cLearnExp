082f3729c810fd093bb46bed83e07eb65a3af1fc
ashishcdev <ashish_kushwaha@outlook.com>
2017-05-11 19:43:58 +0530

    function pointer

--- yello.cpp
+++ yello.cpp
@@ -3,9 +3,19 @@
 
 #include <iostream>
 
+int add(int a, int b) {
+    return (a + b);
+}
+
 int main(int argc, char *argv[]) {
     using namespace std;
     cout << "Yello world!! it is C++ here " << endl;
     cout << "Ashu yaha bhi pagla hai" << endl;
+    printf("Name of the program is %s\n", argv[0]);
+    for (int i = 1; i < argc; i++) {
+	printf("%d th command line argument is\n %s\n", i, argv[i]);
+    }
+    int (*addPt) (int, int) = add;
+    cout << (*addPt) (10, 11) << endl;
     return 0;
 }
