ca5a066bee1b94e2a60718749ae4c5e11dd7d4b9
ashishcdev <ashish_kushwaha@outlook.com>
2017-05-12 08:41:10 +0530

    argument list and function overloading

--- yello.cpp
+++ yello.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 int add(int a, int b) {
     return (a + b);
 }
@@ -13,8 +15,40 @@ int *returnPt(int copyVariable, int padding) {
     return retPt;
 }
 
+void workWithVoidPt(void) {
+    void *voidPt;
+    int dummy1 = 111;
+    char *dummy = "This is dummy";
+    voidPt = (char *) (&dummy);
+    printf
+	("void pointer is pointing to the integer dummy1 at address %d\n",
+	 dummy);
+}
+
+int argList(int a = 10, int b = 19) {
+    return a + b;
+}
+
+void function(int a, int b, int c) {
+    cout << "I was in three arg function with a = " << a << " and b = " <<
+	b << " and c = " << c << endl;
+}
+
+void function(void) {
+    cout << "I was in void type function with the same name" << endl;
+}
+
+void fncOvrlod(void) {
+    function();
+    function(10, 20, 30);
+}
+
+//const int (&functionRefer) (void) {
+  //  int a = 2;
+    //return s;
+//}
+
 int main(int argc, char *argv[]) {
-    using namespace std;
     cout << "Yello world!! it is C++ here " << endl;
     cout << "Ashu yaha bhi pagla hai" << endl;
     printf("Name of the program is %s\n", argv[0]);
@@ -26,5 +60,12 @@ int main(int argc, char *argv[]) {
     cout << (*addPt[0]) (10, 11) << endl;
     cout << "returned value of the copied int variable 100 is " <<
 	(*returnPt) (100, 0) << endl;
+    workWithVoidPt();
+    int s = 100;
+    //int &b = functionRefer();
+    int &b = s;
+    cout << "b containing " << b << endl;
+    cout << "argument list out put " << argList(100, 54) << endl;
+    fncOvrlod();
     return 0;
 }
