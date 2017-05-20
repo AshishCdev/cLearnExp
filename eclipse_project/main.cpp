f8c07bea8cab7cf8de6dc27a4419645ec38b614d
ashishcdev <ashish_kushwaha@outlook.com>
2017-05-20 19:46:08 +0530

    Worked with file

--- eclipse_project/main.cpp
+++ eclipse_project/main.cpp
@@ -1,18 +1,35 @@
-#include<iostream>
-#include<stdarg.h>
+#include <iostream>
+#include <stdarg.h>
+#include <stdio.h>
 
 using namespace std;
 typedef int myInt;
 
 myInt global = 99;
 
-void variArg(int num,...){
+void variArg(myInt num,...){
 	va_list vaList;
 	va_start(vaList,num);
 	for (int i = 0; i < num; ++i) {
 		cout << "arg " << i << " is "<<va_arg(vaList,char *) <<endl;
 	}
 	va_end(vaList);
+}
+
+void workWithFiles(void){
+	FILE *FPT;
+	FPT = fopen("ESP.txt","r");
+	if(FPT == 0) {
+		cout<<"Couldn't open the file"<<endl;
+	}
+	else{
+		while (1) {
+			char out = fgetc(FPT);
+			cout << out ;
+			if(out==EOF) break;
+		}
+	}
+	fclose(FPT);
 }
 
 int main(int argc, char *argv[]){
@@ -24,5 +41,6 @@ int main(int argc, char *argv[]){
 	}
 	cout<<"Again Global int is "<<global<<endl;
 	variArg(4,"String 1","String 2","String 3","String 4");
+	workWithFiles();
 	return 0;
 }
