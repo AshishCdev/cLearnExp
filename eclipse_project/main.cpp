49b9fa47125e2fdb4ab54dec87955f404490be96
ashishcdev <ashish_kushwaha@outlook.com>
2017-05-20 19:16:11 +0530

    used variable argumens in c

--- eclipse_project/main.cpp
+++ eclipse_project/main.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<stdarg.h>
 
 using namespace std;
+typedef int myInt;
+
+myInt global = 99;
+
+void variArg(int num,...){
+	va_list vaList;
+	va_start(vaList,num);
+	for (int i = 0; i < num; ++i) {
+		cout << "arg " << i << " is "<<va_arg(vaList,int) <<endl;
+	}
+	va_end(vaList);
+}
 
 int main(int argc, char *argv[]){
 	cout<<"Care about the accuracy of time"<<endl;
+	cout<<"Global int is "<<global<<endl;
+	{
+		myInt global = 777;
+		cout<<"Local int is"<<global<<endl;
+	}
+	cout<<"Again Global int is "<<global<<endl;
+	variArg(4,34,8,78,89);
 	return 0;
 }
