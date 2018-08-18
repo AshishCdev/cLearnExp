feca9b258b5a49a97f6c956b11c991c3b77f564e
ashishcdev <ashish_kushwaha@outlook.com>
2018-08-19 00:16:22 +0530

    Removed files

--- compileCpp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Indenting......"
-indent -kr -br -ce -brf yello.cpp
-rm yelloCpp
-echo "compiling........"
-g++ -std=c++11 -Wall -g -o yelloCpp yello.cpp
-echo "Running.........."
-./yelloCpp
