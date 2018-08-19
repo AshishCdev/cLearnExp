6e5042b6e2bea717459f60fd80e8d35d3f5fb12f
ashishcdev <ashish_kushwaha@outlook.com>
2018-08-19 21:19:20 +0530

    Created inline function and modified the compile.sh to produce precompiled files

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ echo "indenting......"
 indent -kr -br -ce -brf source.c
 rm sourceObj
 echo "compiling........."
+gcc -E source.c > prcmpldSrc.c
 gcc -std=c99 -Wall -g -o sourceObj source.c
 ./sourceObj
 echo "Done :)"
--- source.c
+++ source.c
@@ -7,6 +7,12 @@
 			printf("a is going to be replaced by %d\n",a*10);\
 		}
 
+inline int myInline(int, int);
+
+int myInline(int a, int b) {
+    return ((a * b) + 2);
+}
+
 int main(int argc, char *argv[]) {
     printf("Starting with %d arguments ...\n", argc);
     while (argc--) {
@@ -16,5 +22,11 @@ int main(int argc, char *argv[]) {
     printf("This is the converted string with macro %s\n",
 	   MAKE_STRING(12345));
     printf("This is added string from macro %d\n", ADD_STRING(123, 879));
+    printf
+	("Calling inline function with %d and %d arguments and returning %d\n",
+	 10, 11, myInline(10, 11));
+    printf
+	("Calling inline function with %d and %d arguments and returning %d\n",
+	 5, 6, myInline(5, 6));
     return 0;
 }
